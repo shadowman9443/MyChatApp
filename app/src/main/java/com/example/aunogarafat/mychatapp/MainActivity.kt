83935bd9bd057fb48519d0bc8c1f9bb9d434416a
aunogarafat <shadowman9443@gmail.com>
2018-01-30 23:49:47 +0600

    Working on MainActivity class ..when user already logged in it will directly go to DashBoardActivity

--- app/src/main/java/com/example/aunogarafat/mychatapp/MainActivity.kt
+++ app/src/main/java/com/example/aunogarafat/mychatapp/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.aunogarafat.mychatapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var mAuth:FirebaseAuth?=null
+    var user:FirebaseUser?=null
+    var mAuthListener:FirebaseAuth.AuthStateListener?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mAuth = FirebaseAuth.getInstance()
+
+        mAuthListener=FirebaseAuth.AuthStateListener {
+            firebaseAuth: FirebaseAuth ->
+            user=firebaseAuth.currentUser
+            if(user!=null){
+                startActivity(Intent(this,DashboardActivity::class.java))
+            }
+        }
         btnLogin.setOnClickListener {
             startActivity(Intent(this,LoginActivity::class.java))
         }
@@ -18,5 +33,17 @@ class MainActivity : AppCompatActivity() {
           startActivity(Intent(this,CreateAccountActivity::class.java))
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mAuth!!.addAuthStateListener(mAuthListener!!)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if(mAuthListener!=null){
+            mAuth!!.removeAuthStateListener(mAuthListener!!)
+        }
     }
 }
