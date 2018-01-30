38ba2ea876037bb5aeeec6484e0eef00ad16683f
aunogarafat <shadowman9443@gmail.com>
2018-01-31 00:50:48 +0600

    Add two fragment and also develop the dashboardactivity

--- app/src/main/java/com/example/aunogarafat/mychatapp/activities/DashboardActivity.kt
+++ app/src/main/java/com/example/aunogarafat/mychatapp/activities/DashboardActivity.kt
@@ -1,15 +1,24 @@
 package com.example.aunogarafat.mychatapp.activities
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.aunogarafat.mychatapp.R
+import com.example.aunogarafat.mychatapp.adapters.SectionPagerAdapter
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        supportActionBar!!.title="Dashboard"
+        var sectionPagedapter:SectionPagerAdapter?=null
+        sectionPagedapter=SectionPagerAdapter(supportFragmentManager)
+        dash_viewPager.adapter=sectionPagedapter
+        main_tab_dash.setupWithViewPager(dash_viewPager)
+        main_tab_dash.setTabTextColors(Color.BLUE,Color.WHITE)
         if(intent.extras!=null){
             var displayName=intent.extras.get("name")
             Toast.makeText(this,"Name"+displayName, Toast.LENGTH_LONG).show()
---
+++ app/src/main/java/com/example/aunogarafat/mychatapp/adapters/SectionPagerAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.aunogarafat.mychatapp.adapters
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import com.example.aunogarafat.mychatapp.fragments.ChatsFragment
+import com.example.aunogarafat.mychatapp.fragments.UsersFragment
+
+/**
+ * Created by aunogarafat on 1/31/18.
+ * Project name MyChatApp
+ */
+class SectionPagerAdapter(fm:FragmentManager):FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+       when(position){
+           0->{
+               return UsersFragment()
+           }
+           1->{
+               return ChatsFragment()
+           }
+       }
+        return null!!
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        when(position){
+            0->{
+                return "Friends"
+            }
+            1->{
+                return "Chats"
+            }
+        }
+        return super.getPageTitle(position)
+    }
+}
