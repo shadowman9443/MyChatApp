885536ab210daf30bb697bf164fe6dc4d8f13ea6
aunogarafat <shadowman9443@gmail.com>
2018-02-05 23:54:16 +0600

    Finished the Chat App.I am so happy.I am not completed the 100% of the project.what i learnt that i get used to with kotlin.Kotlin really is a fun

---
+++ app/src/main/java/com/example/aunogarafat/mychatapp/models/Friendlymessage.kt
@@ -0,0 +1,8 @@
+package com.example.aunogarafat.mychatapp.models
+
+/**
+ * Created by aunogarafat on 2/5/18.
+ * Project name MyChatApp
+ */
+data class Friendlymessage(var id: String?=null, var text: String?=null, var name: String?=null) {
+}
